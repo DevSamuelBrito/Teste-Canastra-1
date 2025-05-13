6aeee231bbba0aa5afe6917425abd7c5efe42c1e
Welisson FAS <welisson@nucleologuin.com.br>
2025-05-12 21:37:21 -0300

    Primeira Postagem

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Olá meu nome é ...</h1>
+      </div>
+      <div className="card">
+        <button>
+          Gerar Número Aleatório
+        </button>
+        <p>
+          MOSRAR AQUI O NÚMERO ALEATÓRIO
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
