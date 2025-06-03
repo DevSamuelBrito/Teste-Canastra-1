41f72cbfd38bd4ff11d40a0a066518f22bf81f31
DevSamuelBrito <samuelbrito.dev@gmail.com>
2025-06-03 10:34:13 -0300

    feat: implementar input para digitar nome com exibição dinamica e tratamento de nome vazio

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
-import './App.css'
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+
   return (
     <>
       <div>
-        <h1>Olá meu nome é ...</h1>
+        <h1>Olá meu nome é {name.trim() === "" ? "..." : name}</h1>
+        <input
+          onChange={(e) => setName(e.target.value)}
+          type="text"
+          placeholder="Digite seu nome aqui"
+          value={name}
+        />
       </div>
       <div className="card">
-        <button>
-          Gerar Número Aleatório
-        </button>
-        <p>
-          MOSRAR AQUI O NÚMERO ALEATÓRIO
-        </p>
+        <button>s Gerar Número Aleatório</button>
+        <p>MOSRAR AQUI O NÚMERO ALEATÓRIO</p>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
